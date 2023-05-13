881e99bb9aafd10d3f68acd2233def68ef491834
Vu Hung <phamvuhung2710@gmail.com>
2023-05-13 22:03:43 +0700

    servlet login with context

---
+++ ServletHelloWorld/src/main/java/controller/HomeServlet.java
@@ -0,0 +1,72 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	 private String usernameCx;
+	 private String passwordCx;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+    	ServletContext context = config.getServletContext();
+    	
+    	usernameCx = context.getInitParameter("username");
+    	passwordCx = context.getInitParameter("password");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if (username.equals(usernameCx) && password.equals(passwordCx)) {
+			response.sendRedirect("success.jsp");
+		} else {
+			response.sendRedirect("failture.jsp");
+		}
+	
+		
+//		request.setAttribute("msg", message);
+//		request.getRequestDispatcher("home.jsp").forward(request,  response);
+		
+//		HttpSession session = request.getSession();
+//		session.setAttribute("msg", message);
+//		response.sendRedirect("home.jsp");
+		
+		
+	}
+
+}
---
+++ ServletHelloWorld/src/main/webapp/login.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="home" method="post">
+	<div>
+		<label>Username</label>
+		<input name="username" type="text" />
+	</div>
+	
+	<div>
+		<label>Password</label>
+		<input name="password" type="text" />
+	</div>
+		
+	<button type="submit">Submit</button>
+	</form>
+</body>
+</html>
